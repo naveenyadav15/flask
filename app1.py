1bf3e7bc51629ea7706d8d127c2a92de4690e8f1
Naveen yadav <naveenyadav15@users.noreply.github.com>
2018-10-14 21:07:33 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def HomePage():
-    return "<h1>Hello Home page</h1>"
+    return "<h1>Hello Naveen </h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
